dc843beb66bda4bf82554ea56a7d913507c8aed5
omgxlori <lbelovin@gmail.com>
2025-10-22 15:15:40 -0400

    wip

--- Magic8Ball/Magic 8 Ball Project/src/App.tsx
+++ Magic8Ball/Magic 8 Ball Project/src/App.tsx
@@ -1,20 +1,55 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-    return (
-        <>
-        <div>
-            <h1>Magic 8 Ball Coming Soon!</h1>
-            </div>
-            <div className="eightball">
-              <div className="triangle">
-                <div className="circle">
-                  <span className="answer-text">8</span>
-                </div>
+export default function App() {
+  const [spinning, setSpinning] = useState(false);
+  // const [showAnswer, setShowAnswer] = useState(false); // optional: to reveal the triangle
+
+  const handleClick = () => {
+    if (spinning) return;
+    setSpinning(true);
+  };
+
+  return (
+    <div className="wrap">
+      <h1>Magic 8 Ball</h1>
+
+      <div
+        className={[
+          "ball",
+          spinning ? "is-spinning" : "",
+          // showAnswer ? "show-answer" : "",
+        ].join(" ")}
+        onClick={handleClick}
+      >
+        {/* glossy highlight is drawn by .ball::after */}
+
+        <div
+          className="roll"
+          onAnimationEnd={() => {
+            setSpinning(false);
+            // toggle answer after each roll if you want:
+            // setShowAnswer((v) => !v);
+          }}
+        >
+          {/* FRONT: 8 side, closer to camera */}
+          <div className="eight3D">
+            <div className="eight"><b>8</b></div>
+          </div>
+
+          {/* BACK: triangle side, deeper inside */}
+          <div className="magic3D">
+            <div className="magic">
+              <div className="answer">
+                <u></u><u></u><span></span>
               </div>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
 
-export default App
+      {/* Optional manual toggle */}
+      {/* <button onClick={() => setShowAnswer(v => !v)} style={{marginTop: 16}}>Toggle answer</button> */}
+    </div>
+  );
+}
